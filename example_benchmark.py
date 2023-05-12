e0b19ae8ec66e9506bc4f7946a53633851ada7c2
hoanduy27 <hoanduy27@gmail.com>
2023-05-12 22:03:18 +0700

    add example for linkage threshold

--- example_benchmark.py
+++ example_benchmark.py
@@ -9,12 +9,21 @@ G = nx.read_gml('graphs/G_dir_alpha0.8_beta1.0.gml')
 bm = Benchmark(
     G, 
     algorithms=[
-        (algorithms.Louvain, {})
+        (algorithms.GirvanNewman, dict(level=3)),
+        (algorithms.Louvain, {}),
+        (algorithms.Leiden, {}),
     ],
-    metrics=[metrics.Modularity],
-    n_iters=10
+    metrics=[
+        (metrics.NumClusters, {}),
+        (metrics.Modularity, {}),
+        (metrics.CDLibMetric, dict(metric='internal_edge_density')),
+        (metrics.CDLibMetric, dict(metric='cut_ratio')),
+    ],
+    n_iters=5,
+    linkage=True,
+    num_thres=20
 )
 
 result = bm.run()
 
-result.to_csv('log/benchmark.csv', index=0)
+result.to_csv('log/benchmark_full.csv', index=0)
